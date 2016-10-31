ceeb7ed29fdf369d0e1c1b82da975c45a09fcd87
Donald Gaxho <dgaxho@thelevelup.com>
2016-10-31 01:58:38 -0400

    Clean up

--- cleanformatter.rb
+++ cleanformatter.rb
@@ -1,17 +1,13 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
-module XCPretty
-  class CleanFormatter < Formatter
-
-    def pretty_format(text)
+class CleanFormatter < XCPretty::Simple
+	def pretty_format(text)
       if text.include? "Can't find keyplane that supports type 4 for keyboard"
         return
       end
       parser.parse(text)
     end
-    
-  end
 end
 
 CleanFormatter
